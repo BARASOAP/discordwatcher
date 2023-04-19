441ad7d37673968562cfa92d9652157d8337602b
BARASOAP <niel.j.barasona@gmail.com>
2023-04-19 11:15:21 -0400

    Add Offline ignore
    Resolves issue where status would change to blank when offline and then
    sends message immediately when they log in.
    
    Update Dotenv
    Update cooldown
    Update Cooldown logic

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,28 @@ import { Activity, Client, IntentsBitField, Message, TextChannel, Collection, Em
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const { WATCHER_ID, CHANNEL_ID, TOKEN } = process.env;
+
+if (!WATCHER_ID || !CHANNEL_ID || !TOKEN) {
+  console.error("Error: Required environment variables WATCHER_ID, CHANNEL_ID, and TOKEN are not set.");
+  process.exit(1);
+}
+
+const watcher = WATCHER_ID;
+const channelID = CHANNEL_ID;
+const EMBED_COLOR = 0x0099FF;
+const EMBED_FOOTER_TEXT = 'TAKASUI NEWS NETWORK';
+
 const client = new Client({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages, IntentsBitField.Flags.MessageContent, IntentsBitField.Flags.GuildPresences] });
-
-// User ID of the watcher
-const watcher = process.env.WATCHER_ID
-
-// Channel ID to send the message to
-const channelID = process.env.CHANNEL_ID
 
 // Cooldown for Presence Update
 const cooldown = new Collection<string, number>();
-const cooldownDuration = 15 * 60 * 1000; 
+const cooldownDuration = 1 * 1 * 1000; 
+
+const newsEmbed = new EmbedBuilder()
+  .setColor(EMBED_COLOR)
+  .setTimestamp(Date.now())
+  .setFooter({ text: EMBED_FOOTER_TEXT });
 
 client.once('ready', () => {
   console.log(`Logged in as ${client.user!.tag}!`);
@@ -20,8 +31,8 @@ client.once('ready', () => {
 
 client.on('presenceUpdate', (oldPresence, newPresence) => {
   if (!newPresence) return; // If the new presence is null, do nothing.
-
-  if (newPresence.user!.id !== watcher) return; // If the user is not the watcher, do nothing.
+  else if (newPresence.user!.id !== watcher) return; // If the user is not the watcher, do nothing.
+  else if (oldPresence?.status === 'offline' || newPresence.status === 'online') return;
 
   const oldStatus = oldPresence?.activities[0]?.state ?? null; // Get the old status, or use 'offline' if it's not available.
   const newStatus = newPresence.activities[0]?.state; // Get the new status.
@@ -40,17 +51,12 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
       const timeLeft = ((cooldownDuration - (now - lastUpdate)) / 1000).toFixed(1);
       console.log(`${timeLeft} until available`);
       return;
+    } else {
+      cooldown.set('presenceUpdate', now);
+      setTimeout(() => cooldown.delete('presenceUpdate'), cooldownDuration);
     }
 
-    cooldown.set('presenceUpdate', now);
-    setTimeout(() => cooldown.delete('presenceUpdate'), cooldownDuration);
-
-    const newsEmbed = new EmbedBuilder()
-      .setColor(0x0099FF)
-      .setTitle(`${newStatus}`)
-      .setTimestamp(Date.now())
-      .setFooter({ text: 'TAKASUI NEWS NETWORK' });
-
+    newsEmbed.setTitle(`${newStatus}`)
     channel.send({content: `@everyone`, embeds: [newsEmbed] });
     
     console.log(
@@ -59,4 +65,4 @@ client.on('presenceUpdate', (oldPresence, newPresence) => {
   }
 });
 
-client.login(process.env.TOKEN);
+client.login(TOKEN);
